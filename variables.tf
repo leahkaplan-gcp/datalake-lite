d4514ede1762c09cdac952c436ced3af747ff06f
leahkaplan-gcp <63087107+leahkaplan-gcp@users.noreply.github.com>
2021-03-20 16:57:44 +0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project" { }
+
+variable "credentials_file" { }
+
+variable "region" {
+  default = "australia-southeast1"
+}
+
+variable "zone" {
+  default = "australia-southeast1-c"
+}
