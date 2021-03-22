1d8d1a11fd113f1c701c238515a99e06ee645156
leahkaplan-gcp <63087107+leahkaplan-gcp@users.noreply.github.com>
2021-03-22 14:17:58 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "3.59.0"
+      version = "3.59.0" 
     }
   }
 }
+
 
 provider "google" {
 
@@ -16,24 +17,44 @@ provider "google" {
   zone    = var.zone
 }
 
+resource "google_project_service" "ai-notebook-service" {
+  project = var.project
+  service = "notebooks.googleapis.com"
+
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "resourcemanager-service" {
+  project = var.project
+  service = "cloudresourcemanager.googleapis.com"
+
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "monitoring-service" {
+  project = var.project
+  service = "monitoring.googleapis.com"
+
+  disable_dependent_services = true
+}
+
+
 resource "google_bigquery_dataset" "dataset" {
   dataset_id                  = "datalake"
   friendly_name               = "datalake"
   location                    = var.region
 }
 
-resource "random_string" "bucket" {
-  length  = 8
-  special = false
-  upper   = false
-}
 
 resource "google_storage_bucket" "bucket" {
-  name     = "${var.project}-${random_string.bucket.result}"
+  name     = "${var.project}-datalake-storage"
   location = var.region
 }
 
 resource "google_notebooks_instance" "instance" {
+  depends_on = [google_project_service.ai-notebook-service]
+
+  count = var.create_ai_notebook #conditional creation - if this is 1, will create, if 0, then no
   name = "ai-notebook"
   location = var.zone
   machine_type = "n1-standard-4"
@@ -42,5 +63,5 @@ resource "google_notebooks_instance" "instance" {
     image_family = "tf-latest-cpu"
   }
 
-  desired_status = "TERMINATED"
 }
+
